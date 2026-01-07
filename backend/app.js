2a61c3c2c9d220d64115e616f44655509e2add5d
krishnagupta031003-ops <krishnagupta031003>
2026-01-07 23:56:19 +0530

    database connect

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const { connectDB } = require("./config/db");
+
+const authRoute = require("./routes/auth.routes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectDB();
+
+app.use("/Auth", authRoute);
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+  res.send("i am the dashboard");
 });
 
 app.listen(3000);
---
+++ backend/config/env.js
@@ -0,0 +1,13 @@
+require("dotenv").config({});
+
+const { PORT, MONGO_URI, JWT_SECRET } = process.env;
+
+// if (!MONGO_URI || !JWT_SECRET) {
+//   throw new Error('Missing required ENV variables');
+// }
+
+module.exports = {
+  PORT,
+  MONGO_URI,
+  JWT_SECRET,
+};
