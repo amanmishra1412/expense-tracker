33dd948a9f53e04c19130b40c71055534ab7a656
amanmishra1412 <mishra06aman@gmail.com>
2026-01-08 23:58:46 +0530

    navbar add

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='px-4 py-3 flex justify-center items-center'>
+      <h2 className='text-2xl font-semibold mr-10'>Expense Tracker</h2>
+      <nav className='flex items-center gap-3'>
+        <NavLink to='/dashboard'>Home</NavLink>
+        <NavLink to='/income'>Income</NavLink>
+        <NavLink to='/expense'>Expense</NavLink>
+      </nav>
+    </header>
   )
 }
 
