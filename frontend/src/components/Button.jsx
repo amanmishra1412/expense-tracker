261439f4a137f45056a1735057e9e80866ed2371
amanmishra1412 <mishra06aman@gmail.com>
2026-01-21 15:04:45 +0530

    change in button

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ name, active, onclick }) => {
         <button
             onClick={onclick}
             className={`
-                w-full md:w-auto px-4 py-2 rounded-xl font-medium capitalize
+                w-full px-4 py-2 rounded-xl font-medium capitalize
                 transition-all duration-200 cursor-pointer
                 ${
                     active
